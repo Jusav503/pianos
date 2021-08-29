8198394c66aec5d76f6aff8a730981d4a8981213
Justice Velasco <justicevgomez001@gmail.com>
2021-08-29 20:43:37 +0200

    articulos en teclados electronicos

--- resources/views/pages/teclados-electronicos.blade.php
+++ resources/views/pages/teclados-electronicos.blade.php
@@ -6,10 +6,35 @@
 @section('image', '../images/teclado-electronico.jpg')
 
 @section('MainContent')
-    <div class="container">
-        <article class="text-gray-500">
-            <h2 class="titles text-3xl">Los mejores teclados electrónicos del 2021 🎹🔥</h2>
-            <p>Antes de comenzar tienes que tener en cuenta que pianos y teclados no son lo mismo. Existe una diferencia entre teclados y pianos una de ella es el número de teclas del instrumento. El teclado puede tener desde 49 teclas y 4 octavas hasta 76 teclas y 6 octavas, mientras que el piano digital dispone de 88 teclas y poco más de 7 octavas</p>
+    <div class="container text-gray-500">
+        <article>
+            <h2 class="titles text-3xl">¡Los mejores teclados electrónicos del 2021! 🎹🔥</h2>
+            <p>Antes de comenzar tienes que tener en cuenta que pianos y teclados no son lo mismo. Existe una diferencia entre teclados y pianos, una de ella es el número de teclas del instrumento, el teclado puede tener desde 49 teclas y 4 octavas hasta 76 teclas y 6 octavas, mientras que el piano digital dispone de 88 teclas y poco más de 7 octavas.</p>
+        </article>
+        <article>
+            <h2 class="titles text-2xl">¿Qué es un teclado electrónico?</h2>
+            <img src="{{asset('../images/teclado-electronico.jpg')}}" width="400px" alt="teclado electrónico">
+            <p>Un teclado electrónico o piano digital es un instrumento musical, en algunos casos se basa en mecanismos eléctricos, electrónicos o digitales que crean los sonidos. Es aquí donde se encuentran los sintetizadores como otros instrumentos creados para imitar pianos mediante muestras musicales de sonidos previamente grabados. Dichos sonidos pueden venir fijos de fabrica, o ser capturados y manipulados mediante un sampleador.</p>
+            <br>
+            <p>También hay teclados que combinan facetas, incluyendo tanto instrumentos como pueden ser la guitarra, el piano, la trompeta, etc., como sonidos de palmadas, números en inglés o castellano. Los teclados electrónicos completos de piano, incluyen 88 teclas, 36 negras y 52 blancas. Si además incluye pedales, se puede considerar un piano electrónico.</p>
+        </article>
+        <article class="grid grid-col lg:grid-cols-3 p-4 justify-items-center gap-10">
+            <div class="col-span-3 text-center">
+                <h2 class="titles text-2xl">Tipos de teclados electrónicos</h2>
+                <p>Tienes que tener en cuenta que en el mundo de los teclados electrónicos o digitales existen tres principales tipos: los portátiles o de escenario, comtemporáneos y verticales.</p>
+            </div>
+            <div class="flex flex-col">
+                <img src="{{asset('../images/piano-portatil.jpg')}}" class="w-full" alt="piano portátil">
+                <p>- Como su nombre lo indica los pianos portatiles o de escenario suelen ser de fácil movilidad y pueden proporcionar un sonido mucho más fuerte para los músicos que esperan más volumen.</p>
+            </div>
+            <div class="flex flex-col">
+                <img src="{{asset('../images/piano-contemporaneo.jpg')}}" class="w-full" alt="piano contemporaneo">
+                <p>- Los pianos digitales del tipo contemporáneos se caracterizan por tener un aspecto mucho más moderno y menos apegado a un piano de cola convencional</p>
+            </div>
+            <div class="flex flex-col">
+                <img src="{{asset('../images/piano-vertical.jpg')}}" class="w-full" alt="piano vertical">
+                <p>- Por su parte los pianos verticales tienen la peculiaridad de parecerse a un piano acústico con altavoces integrados en la caja del soporte</p>
+            </div>
         </article>
     </div>
 @endsection
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -15,14 +15,14 @@
     </div>    
 
     {{-- Content --}}
-    <div class="container">
+    <div class="container text-gray-500">
         
-        <article class="text-gray-500">
+        <article>
             <h1 class="text-3xl titles">¿Qué es un piano?</h1>
             <p>Muchas personas lo describen como un instrumento musical grande de color negro con teclas y que al tocarlo se escucha bonito, y aunque están en lo cierto un piano es mucho más que eso. Tocar el piano aumenta la coordinación entre cuerpo y cerebro. Por norma general, aprender a tocar un instrumento musical contribuye al desarrollo de habilidades motoras y de coordinación.</p>
         </article>        
 
-        <article class="text-gray-500">
+        <article>
             <h2 class="text-2xl titles">Historia del piano</h2>
             <p>Inventado en Italia por Bartolomeo Cristofori alrededor del año 1700 aproximadamente, es un instrumento acústico de cuerdas en el que las cuerdas son golpeadas por <a href="" class="text-blue-400">martillos de madera</a> recubiertos con un material más suave (los martillos modernos están cubiertos con densos fieltros de lana; algunos pianos antiguos usaban cuero). Se toca usando un teclado, que es una fila de teclas o pequeñas palancas que el ejecutante presiona o golpea con los dedos y pulgares de ambas manos para hacer que los martillos golpeen las cuerdas.
                 <br>
@@ -32,33 +32,33 @@
             <span>(Primer piano construido en 1695)</span>
         </article>
         
-        <article class="text-gray-500">
+        <article>
             <h2 class="text-2xl titles">Tipos de piano</h2>
             <p>Existen tres principales tipos de pianos: <span class="text-black">piano de cola, el vertical o de pared y el digital</span> <a href="" class="text-blue-400">El piano de cola</a> es el más grande de los tres. En el interior de su elegante diseño guarda la caja de resonancia y las cuerdas de forma horizontal. En función de su tamaño se pueden encontrar el piano de cola mignon, un cuarto de cola, media cola, tres cuartos de cola y el de gran cola.</p>
         </article>
 
-        <article class="text-gray-500">
+        <article>
             <h2 class="titles text-2xl">Piano de cola</h2>
             <p>Está compuesto por una caja de resonancia a la que se ha agregado un teclado mediante el cual se percuten las cuerdas de acero con martillos forrados de fieltro, produciendo el sonido. Las vibraciones se transmiten a través de los puentes a la tabla armónica, que las amplifica. Está formado por un arpa cromática de cuerdas múltiples, accionada por un mecanismo de percusión indirecta, a la que se le han añadido apagadores. Fue inventado en torno al año 1700 por el paduano Bartolomeo Cristofori. Entre sus antecesores se encuentran instrumentos como la cítara, el monocordio, el dulcémele, el clavicordio y el clavecín (del que deriva la forma, pero no la mecánica).</p>
             <br>
             <img src="{{asset('../images/piano-de-cola.webp')}}" alt="piano de cola">
         </article>
 
-        <article class="text-gray-500">
+        <article>
             <h2 class="titles text-2xl">Piano vertical o de pared</h2>
             <p>Es uno de los dos tipos de pianos conocidos como acústicos modernos junto al piano de cola. ... La principal característica de este tipo de piano viene dada por la posición de la caja de resonancia, situada en un plano vertical, así como las cuerdas.</p>
             <br>
             <img src="{{asset('../images/piano-de-pared.jpg')}}" width="400px" alt="piano de pared">
         </article>
 
-        <article class="text-gray-500">
+        <article>
             <h2 class="titles text-2xl">Piano digital</h2>
             <p>Un piano electrónico o piano digital es un instrumento musical eléctrico de teclado que produce sonido mecánicamente y lo convierte en una señal de audio electrónica mediante pastillas (pickups). A diferencia del sintetizador, el piano eléctrico no es un instrumento electrónico, sino electromecánico.</p>
             <br>
             <img src="{{asset('../images/piano-electrico.jpg')}}" width="400px" alt="piano electrico">
         </article>
 
-        <article class="text-gray-500">
+        <article>
             <h2 class="titles text-2xl">Precios</h2>
             <p>Ahora bien, hablemos de precios. En primer lugar, hay que decir que hablar de pianos es como hablar de las personas, hay muchos tipos diferentes de pianos, y además cada tipo de piano está recomendado para un uso y nivel concreto, por esto podríamos decir que el precio de un piano puede variar entre los 300€ si se trata de un piano digital o eléctrico y hasta los 10.000€ o incluso más tratándose de pianos de cola.</p><br>
             <p>Pero no te preocupes nosotros tenemos una guia completa para ayudarte a que compres el mejor piano adaptado a tu nivel y a tu bolsillo.</p>
