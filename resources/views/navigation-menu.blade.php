11c7e2fca5451adc7af1b4a70fa97491f44ae8cd
Justice Velasco <justicevgomez001@gmail.com>
2021-08-30 19:33:55 +0200

    header updated

--- resources/views/navigation-menu.blade.php
+++ resources/views/navigation-menu.blade.php
@@ -2,20 +2,18 @@
     <!-- Primary Navigation Menu -->
     <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div class="flex justify-between h-16">
-            <div class="flex">
-                <!-- Logo -->
-                <div class="flex-shrink-0 flex items-center">
-                    <a href="{{ route('home') }}">
-                        <x-jet-application-mark class="block h-9 w-auto" />
-                    </a>
-                </div>
+            <!-- Logo -->
+            <div class="flex-shrink-0 flex items-center">
+                <a href="{{ route('home') }}">
+                    <x-jet-application-mark class="block h-9 w-auto" />
+                </a>
+            </div>
 
-                <!-- Navigation Links -->
-                <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
-                    <x-jet-nav-link href="{{ route('teclados.electronicos') }}" :active="request()->routeIs('teclados.electronicos')" class="text-white">
-                        {{ __('Teclados Electrónicos') }}
-                    </x-jet-nav-link>
-                </div>
+            <!-- Navigation Links -->
+            <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
+                <x-jet-nav-link href="{{ route('teclados.electronicos') }}" :active="request()->routeIs('teclados.electronicos')" class="text-white">
+                    {{ __('Teclados Electrónicos') }}
+                </x-jet-nav-link>
             </div>
 
             <!-- Hamburger -->
